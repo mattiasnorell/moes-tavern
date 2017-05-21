d5b1e2da17499bd4d3001c771928c51a060b5ccf
Mattias Norell <mattias.norell@gmail.com>
2017-05-21 21:53:21 +0200

    Added styles to list item component

--- moes-tavern/src/app/cellar-list-item/cellar-list-item.component.ts
+++ moes-tavern/src/app/cellar-list-item/cellar-list-item.component.ts
@@ -3,7 +3,7 @@ import { ICellarItem } from "app/icellar-item";
 import { CellarListService } from "app/cellar-list/cellar-list.service";
 
 @Component({
-  selector: '[cellar-list-item]',
+  selector: 'cellar-list-item',
   templateUrl: './cellar-list-item.component.html',
   styleUrls: ['./cellar-list-item.component.scss'],
   providers: [CellarListService]
@@ -12,7 +12,15 @@ export class CellarListItemComponent implements OnInit {
 
   constructor(private cellarService:CellarListService) { }
 
+  private currentDate: Date = new Date();
+  private currentDateTimestamp: number = this.currentDate.getTime();
+  private closeToDateLimit: number = this.currentDate.setDate(this.currentDate.getDate() + 7);
+  closeToDate:boolean = false;
+  pastDate:boolean = false;
+
   ngOnInit() {
+    this.closeToDate = new Date(this.item.bestBefore).getTime() < this.closeToDateLimit;
+    this.pastDate = new Date(this.item.bestBefore).getTime() < this.currentDateTimestamp;
   }
 
   @Input() item:ICellarItem;
